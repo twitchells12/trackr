39b5bac357f3f897fc68839559a26afe268e3d05
Steven Tardiff <tardiff.steven@gmail.com>
2020-08-26 13:38:35 -0400

    fixed settings

--- trackr/settings.py
+++ trackr/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .secret_settings import *
+import environ
+# from .secret_settings import *
+env= environ.Env(
+    DEBUG=(bool,False)
+)
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
